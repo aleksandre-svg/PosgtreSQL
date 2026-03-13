280abb511d1bba9a46f205007a134ec6ac925b79
Diana <140351052+Diana044@users.noreply.github.com>
2026-03-14 01:00:01 +0400

    commit

---
+++ public/login/script.js
@@ -0,0 +1,27 @@
+const form = document.getElementById("form")
+
+function User(username, password){
+    this.username = username
+    this.password = password
+}
+
+const sendData = (e) => {
+    e.preventDefault()
+    // PayLoad
+    const info = new User(e.target.username.value, e.target.pass.value)
+
+
+    fetch('http://localhost:3000/login', {
+        method: "POST",
+        headers: {"Content-Type":"application/json"},
+        body: JSON.stringify(info)
+    }).then(res => res.text())
+        .then(data => {
+            data = JSON.parse(data)
+            console.log(data.status)
+
+            alert(data.message)
+        })
+}
+
+form.addEventListener('submit', sendData)
---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require("express")
+const pool = require("./db");
+const PORT = process.env.PORT || 3000
+const app = express()
+require("dotenv").config();
+
+
+app.use(express.json())
+
+app.use("/", express.static("public/home"));
+
+app.use("/login", express.static("public/login"));
+
+
+const validateUser = async (req, res, next) => {
+    const { username, password } = req.body
+
+    try{
+        console.log("User validated")
+        res.json({
+            status: 200,
+            message: "Succesfuly loged in"
+        })
+
+        const result = await pool.query(
+            "INSERT INTO users_accounts (username, password) VALUES ($1, $2) RETURNING *",
+            [username, password]
+        );
+        
+        res.json(result.rows[0]);
+        next()
+    }catch(err) {
+        return res.status(500).json({
+            status: 500,
+            message: "server error"
+        })
+    }
+}
+
+app.get("/check-db", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT current_database()");
+        res.send(result.rows[0]);
+    } catch (err) {
+        console.error("Database connection error:", err.message);
+        res.status(500).send("DB connection failed: " + err.message);
+    }
+});
+
+app.post("/login", validateUser)
+
+
+app.listen(PORT, () => {
+    console.log(`Running on PORT:${PORT}`)
+})
